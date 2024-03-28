e0161a126c4b3065195152bc5cb71e55e414f203
PRoy <sam469roger@gmail.com>
2024-03-28 18:11:34 +0530

    dynamic routes with url parameters

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css';
 
 const formatDate = (date) =>
@@ -7,13 +8,25 @@ const formatDate = (date) =>
     year: 'numeric',
   }).format(new Date(date));
 
-function CityItem({ city: { cityName, emoji, date } }) {
+const flagemojiToPNG = (flag) => {
+  const countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+    .join('');
+
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+  );
+};
+
+function CityItem({ city: { cityName, emoji, date, id } }) {
+  return (
+    <li>
+      <Link to={`${id}`} className={styles.cityItem}>
+        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
