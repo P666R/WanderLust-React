ae21abacf42d1bf4379c178af1624cde0b26be8a
PRoy <sam469roger@gmail.com>
2024-03-30 22:26:32 +0530

    implemented useCities hook for consuming CitiesContext

--- src/hooks/UseCities.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { CitiesContext } from '../contexts/CitiesContext';
-
-export function useCities() {
-  const context = useContext(CitiesContext);
-  return context;
-}
